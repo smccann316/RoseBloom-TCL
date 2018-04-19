d9813cace2497876ae77359447cb86829372da11
Steve <Steve.mccann@live.com>
2018-04-19 11:53:27 -0400

    added missing files from last commit

---
+++ src/RoseBloom.Common.TCL/RoseBloom.Common.TCL/EmotionImpact.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace RoseBloom.Common.TCL
+{
+    /// <summary>
+    /// The emotinal 
+    /// </summary>
+    public class EmotionImpact
+    {
+        public string EmotionId { get; set; }
+        public int Impact { get; set; }
+    }
+}
---
+++ src/RoseBloom.Common.TCL/RoseBloom.Common.TCL/SensesThought.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoseBloom.Common.TCL
+{
+    public class SensesThought : Thought
+    {
+        public Action ThoughtAction { get; set; }
+        public List<Person> ThoughtWith { get; set; }
+        
+
+    }
+}
