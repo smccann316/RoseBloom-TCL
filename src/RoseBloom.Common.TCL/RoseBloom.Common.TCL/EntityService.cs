3945f88be3ef5a5ae52e141dfd8aee25a4df4abb
Steve <Steve.mccann@live.com>
2018-04-24 10:01:09 -0400

    Moved the services to this library from data

---
+++ src/RoseBloom.Common.TCL/RoseBloom.Common.TCL/EntityService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoseBloom.Common.TCL
+{
+    public class EntityService<T> : IEntityService where T : Entity
+    {
+        Type IEntityService.EntityType => typeof(T);
+
+        Task<Entity> IEntityService.AddAsync(Entity toAdd)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> IEntityService.DeleteAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<List<Entity>> IEntityService.GetAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Entity> IEntityService.GetByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Entity> IEntityService.UpdateAsync(Entity toUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ src/RoseBloom.Common.TCL/RoseBloom.Common.TCL/IEntityService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoseBloom.Common.TCL
+{
+    public interface IEntityService
+    {
+        Type EntityType { get; }
+        Task<List<Entity>> GetAsync();
+        Task<Entity> GetByIdAsync(string id);
+        Task<Entity> AddAsync(Entity toAdd);
+        Task<Entity> UpdateAsync(Entity toUpdate);
+        Task<bool> DeleteAsync(string id);
+    }
+}
